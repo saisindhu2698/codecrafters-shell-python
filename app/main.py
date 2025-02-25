6a0c8cc7f5d0b4388287263dc647c4d97c10f291
saisindhu2698 <sindhu.muppaneni@gmail.com>
2025-02-24 18:00:58 -0800

    [multiple]

--- app/main.py
+++ app/main.py
@@ -1,123 +1,126 @@
-app/main.py
-from collections.abc import Mapping
-import readline
-import shlex
-import subprocess
+# main.py
+import platform
 import sys
-import pathlib
-import os
-from typing import Final, TextIO
-SHELL_BUILTINS: Final[list[str]] = [
-    "echo",
-    "exit",
-    "type",
-    "pwd",
-    "cd",
-]
-def parse_programs_in_path(path: str, programs: dict[str, pathlib.Path]) -> None:
-    """Creates a mapping of programs in path to their paths"""
-    for p, _, bins in pathlib.Path(path).walk():
-        for b in bins:
-            programs[b] = p / b
-def generate_program_paths() -> Mapping[str, pathlib.Path]:
-    programs: dict[str, pathlib.Path] = {}
-    for p in (os.getenv("PATH") or "").split(":"):
-        parse_programs_in_path(p, programs)
-    return programs
-PROGRAMS_IN_PATH: Final[Mapping[str, pathlib.Path]] = {**generate_program_paths()}
-COMPLETIONS: Final[list[str]] = [*SHELL_BUILTINS, *PROGRAMS_IN_PATH.keys()]
-def display_matches(substitution, matches, longest_match_length):
-    print()
-    if matches:
-        print("  ".join(matches))
-    print("$ " + substitution, end="")
-def complete(text: str, state: int) -> str | None:
-    matches = list(set([s for s in COMPLETIONS if s.startswith(text)]))
-    if len(matches) == 1:
-        return matches[state] + " " if state < len(matches) else None
-    return matches[state] if state < len(matches) else None
-readline.set_completion_display_matches_hook(display_matches)
-readline.parse_and_bind("tab: complete")
-readline.set_completer(complete)
+from typing import Optional, TextIO
+from .command_factory import CommandFactory
+from .input_parser import InputParser, ParsedInput
+from .shell_context import ShellContext
+if platform.system == "Windows":
+    from pyreadline3 import Readline
+else:
+    import readline
+class Shell:
+    def __init__(self) -> None:
+        self._ctx = ShellContext()
+        self._factory = CommandFactory(self._ctx)
+        self._parser = InputParser()
+        if platform.system == "Windows":
+            self._readline = Readline()
+            self._readline.parse_and_bind("tab: complete")
+            self._readline.set_completer(self.complete)
+        else:
+            readline.parse_and_bind("tab: complete")  # type: ignore
+            readline.set_completer(self.complete)  # type: ignore
+            readline.set_completion_display_matches_hook(self.display_matches)  # type: ignore  # noqa: F821
+            readline.parse_and_bind("set bell-style audible")  # type: ignore
+            readline.set_auto_history(True)  # type: ignore
+    def display_matches(
+        self, substitution: str, matches: list[str], longest_match_length: int
+    ):
+        try:
+            sys.stdout.write("\n")
+            sys.stdout.write(" ".join(matches) + "\n")
+            sys.stdout.write(f"$ {substitution}")
+            sys.stdout.flush()
+            readline.redisplay()  # type: ignore
+        except Exception as e:
+            sys.stderr.write(f"{e}")
+    def complete(self, text: str, state: int) -> Optional[str]:
+        ALL_COMMANDS = (
+            self._ctx.get_built_in_commands() + self._ctx._utils.get_exe_list()
+        )
+        matches = [cmd + " " for cmd in ALL_COMMANDS if cmd.startswith(text)]
+        return matches[state] if state < len(matches) else None
+    def handle_external_command(
+        self,
+        command_name: str,
+        args: list[str],
+        output_stream: Optional[TextIO] = sys.stdout,
+        err_stream: Optional[TextIO] = sys.stderr,
+    ) -> None:
+        subprocess = self._ctx._utils.get_subprocess()
+        is_executable, _ = self._ctx._utils.search_for_exe(command_name)
+        if not is_executable:
+            if err_stream:
+                err_stream.write(f"{command_name}: not found\n")
+        else:
+            command_args = [command_name] + args
+            subprocess.call(command_args, stdout=output_stream, stderr=err_stream)
+    def execute_parsed_input(self, parsed_input: ParsedInput):
+        command = self._factory.create_command(
+            parsed_input.command_name, parsed_input.args
+        )
+        output_stream = sys.stdout
+        err_stream = sys.stderr
+        # handle redirection and appending of stdout and stderr
+        if parsed_input.redirect_to_stdout:
+            if (
+                parsed_input.redirection_symbol == ">>"
+                or parsed_input.redirection_symbol == "1>>"
+            ):
+                output_stream = open(parsed_input.redirect_to_stdout, "a+")
+            else:
+                output_stream = open(parsed_input.redirect_to_stdout, "w")
+        if parsed_input.redirect_to_stderr:
+            if parsed_input.redirection_symbol == "2>>":
+                err_stream = open(parsed_input.redirect_to_stderr, "a+")
+            else:
+                err_stream = open(parsed_input.redirect_to_stderr, "w")
+        try:
+            if command:
+                # explicitly handle exit command
+                if parsed_input.command_name == "exit":
+                    if parsed_input.redirect_to_stdout:
+                        output_stream.close()
+                    if parsed_input.redirect_to_stderr:
+                        err_stream.close()
+                    command.execute()
+                    return 0
+                # else handle the command
+                command.execute(output_stream=output_stream, err_stream=err_stream)
+            elif (
+                # handle external commands
+                parsed_input.command_name not in self._ctx.get_built_in_commands()
+                and parsed_input.args
+                and isinstance(parsed_input.args, list)
+            ):
+                self.handle_external_command(
+                    parsed_input.command_name,
+                    parsed_input.args,
+                    output_stream,
+                    err_stream,
+                )
+            else:
+                if err_stream:
+                    err_stream.write(
+                        f"{parsed_input.command_name}: command not found\n"
+                    )
+        finally:
+            if parsed_input.redirect_to_stdout:
+                output_stream.close()
+            if parsed_input.redirect_to_stderr:
+                err_stream.close()
 def main():
+    shell = Shell()
+    parser = InputParser()
     while True:
         sys.stdout.write("$ ")
-        cmds = shlex.split(input())
-        out = sys.stdout
-        err = sys.stderr
-        close_out = False
-        close_err = False
+        # TODO: implement autocomplete
         try:
-            if ">" in cmds:
-                out_index = cmds.index(">")
-                out = open(cmds[out_index + 1], "w")
-                close_out = True
-                cmds = cmds[:out_index] + cmds[out_index + 2 :]
-            elif "1>" in cmds:
-                out_index = cmds.index("1>")
-                out = open(cmds[out_index + 1], "w")
-                close_out = True
-                cmds = cmds[:out_index] + cmds[out_index + 2 :]
-            if "2>" in cmds:
-                out_index = cmds.index("2>")
-                err = open(cmds[out_index + 1], "w")
-                close_err = True
-                cmds = cmds[:out_index] + cmds[out_index + 2 :]
-            if ">>" in cmds:
-                out_index = cmds.index(">>")
-                out = open(cmds[out_index + 1], "a")
-                close_out = True
-                cmds = cmds[:out_index] + cmds[out_index + 2 :]
-            elif "1>>" in cmds:
-                out_index = cmds.index("1>>")
-                out = open(cmds[out_index + 1], "a")
-                close_out = True
-                cmds = cmds[:out_index] + cmds[out_index + 2 :]
-            if "2>>" in cmds:
-                out_index = cmds.index("2>>")
-                err = open(cmds[out_index + 1], "a")
-                close_err = True
-                cmds = cmds[:out_index] + cmds[out_index + 2 :]
-            handle_all(cmds, out, err)
-        finally:
-            if close_out:
-                out.close()
-            if close_err:
-                err.close()
-def handle_all(cmds: list[str], out: TextIO, err: TextIO):
-    # Wait for user input
-    match cmds:
-        case ["echo", *s]:
-            out.write(" ".join(s) + "\n")
-        case ["type", s]:
-            type_command(s, out, err)
-        case ["exit", "0"]:
-            sys.exit(0)
-        case ["pwd"]:
-            out.write(f"{os.getcwd()}\n")
-        case ["cd", dir]:
-            cd(dir, out, err)
-        case [cmd, *args] if cmd in PROGRAMS_IN_PATH:
-            process = subprocess.Popen([cmd, *args], stdout=out, stderr=err)
-            process.wait()
-        case command:
-            out.write(f"{' '.join(command)}: command not found\n")
-def type_command(command: str, out: TextIO, err: TextIO):
-    if command in SHELL_BUILTINS:
-        out.write(f"{command} is a shell builtin\n")
-        return
-    if command in PROGRAMS_IN_PATH:
-        out.write(f"{command} is {PROGRAMS_IN_PATH[command]}\n")
-        return
-    out.write(f"{command}: not found\n")
-def cd(path: str, out: TextIO, err: TextIO) -> None:
-    if path.startswith("~"):
-        home = os.getenv("HOME") or "/root"
-        path = path.replace("~", home)
-    p = pathlib.Path(path)
-    if not p.exists():
-        out.write(f"cd: {path}: No such file or directory\n")
-        return
-    os.chdir(p)
+            input_line = input()
+            parsed_input = parser.parse(input_line)
+            shell.execute_parsed_input(parsed_input)
+        except EOFError:
+            break
 if __name__ == "__main__":
     main()
