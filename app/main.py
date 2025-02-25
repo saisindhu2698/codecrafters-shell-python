3a5c2d0b7f212cbc4900d2b8358b48375e9e1755
saisindhu2698 <sindhu.muppaneni@gmail.com>
2025-02-24 18:09:12 -0800

    [multiple]

--- app/main.py
+++ app/main.py
@@ -49,12 +49,11 @@ def completer(text, state):
                 return None
 
     elif len(matches) == 1:
-        if state < len(matches):
-            return matches[state] + " "  # Add a space after single completion
-        else:
-            return None
+        
+        return matches[state] if state < len(matches) else None
 
     return None
+
 
 def main():
     global tab_pressed
